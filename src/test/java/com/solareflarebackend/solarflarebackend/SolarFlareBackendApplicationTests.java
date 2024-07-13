5901eddd76d08da069f41c2ff947cda948ed4e3d
Hamza Zafar <hamzazafar99p@gmail.com>
2024-07-14 01:58:43 +0500

    solare-flare-backend

---
+++ src/test/java/com/solareflarebackend/solarflarebackend/SolarFlareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solareflarebackend.solarflarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarFlareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
